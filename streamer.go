a8b8c55746701cef43ab8a26cd8e03f7eef5b240
slicken <slicken@gmail.com>
2025-04-01 20:08:11 +0200

    update

--- streamer.go
+++ streamer.go
@@ -114,3 +114,51 @@ func (bars Bars) StreamBar() <-chan Bar {
 
 	return c
 }
+
+// StreamBar Interval
+func (bars Bars) StreamBarInterval(start, end time.Time, interval time.Duration) <-chan Bar {
+	c := make(chan Bar, 1)
+
+	// check if we have bars
+	if len(bars) == 0 {
+		defer close(c)
+		return c
+	}
+
+	// check if our time is within bars first last times, adjust if needed
+	if ft := bars.FirstBar().T(); !ft.IsZero() {
+		if start.IsZero() || start.Before(ft) {
+			start = ft
+		}
+	}
+	if lt := bars.LastBar().T(); !lt.IsZero() {
+		if end.IsZero() || end.After(lt) {
+			end = lt
+		}
+	}
+
+	// adjust interval if needed
+	if interval < mindur {
+		interval = mindur
+	}
+	if interval > maxdur {
+		interval = maxdur
+	}
+
+	go func() {
+		// time value witch we will increase on loop
+		dt := start
+		for dt.Before(end) {
+			// add looping interval to time
+			dt = dt.Add(interval)
+
+			_, bar := bars.Find(dt)
+
+			c <- bar
+		}
+		close(c)
+
+	}()
+
+	return c
+}
