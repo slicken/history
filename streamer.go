93293d85c9e5f32db92054aa52075b3f739decba
slicken <slicken@gmail.com>
2025-04-01 19:59:34 +0200

    update

--- streamer.go
+++ streamer.go
@@ -95,3 +95,22 @@ func (bars Bars) StreamInterval(start, end time.Time, interval time.Duration) <-
 
 	return c
 }
+
+// Stream bar
+func (bars Bars) StreamBar() <-chan Bar {
+	c := make(chan Bar, 1)
+
+	if len(bars) == 0 {
+		defer close(c)
+		return c
+	}
+
+	go func() {
+		for i := len(bars) - 1; i >= 0; i-- {
+			c <- bars[i]
+		}
+		close(c)
+	}()
+
+	return c
+}
